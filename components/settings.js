648a1cf4accb4752874abfca14c39acfd71d9e28
flawlesscode254 <denzme414@gmail.com>
2021-05-27 13:47:53 +0300

    third

--- components/settings.js
+++ components/settings.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
-import { KeyboardAvoidingView } from 'react-native'
+import { KeyboardAvoidingView, View } from 'react-native'
 import { ScrollView } from 'react-native'
 import { StyleSheet, Text } from 'react-native'
-import { Input, Button } from 'react-native-elements'
+import { Input, Button, Overlay } from 'react-native-elements'
 import { db } from '../firebase'
 
 const settings = () => {
     const [pin, setPin] = useState()
     const [stream, setStream] = useState([])
+    const [visible, setVisible] = useState(false);
 
-    const Check = () => {
-        db.collection('orders').where('pin', '==', pin).get().then((snapshot) => {
+    const Check = async () => {
+        await setVisible(!visible);
+        await db.collection('orders').where('pin', '==', pin).get().then((snapshot) => {
             snapshot.forEach(doc => {
                 setStream("Your unique code is: " + doc.data().code)
             })
             setPin('')
         })
+        await setVisible(visible);
     }
 
     return (
@@ -28,11 +31,19 @@ const settings = () => {
                 <Input value={pin} onChangeText={(text) => setPin(text)} style={{
                     marginTop: 30
                 }} placeholder="Enter your pin" />
+                
+            </ScrollView>
+            <View>
                 <Button onPress={Check} title="Submit" />
                 <Text style={{
                     marginTop: 40,
                 }}>{stream}</Text>
-            </ScrollView>
+            </View>
+            <View>
+                <Overlay isVisible={visible}>
+                    <Text>Checking...</Text>
+                </Overlay>
+            </View>
         </KeyboardAvoidingView>
         
     )
